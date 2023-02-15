a8b31e5836fa9296417307957a8b25009ddf2796
Roman M <rmarchen95@gmail.com>
2023-02-14 18:40:51 -0800

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -17,6 +17,15 @@ bread_router.get('/new', (req, res) => {
     res.render('new')
 })
 
+// EDIT
+bread_router.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
+})
+
+
 
 
 // SHOW
@@ -51,6 +60,18 @@ bread_router.delete('/:indexArray', (req, res) => {
     Bread.splice(req.params.indexArray, 1)
     res.status(303).redirect('/breads')
 })
+
+// UPDATE
+bread_router.put('/:arrayIndex', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
 
 
 
